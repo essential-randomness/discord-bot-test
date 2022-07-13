e5141aee5ad1ffc11fda06463d3da123db5298fd
Essential Randomness <essential.randomn3ss@gmail.com>
2022-07-13 14:00:43 -0700

    Register pinging command

--- index.ts
+++ index.ts
@@ -1,14 +1,59 @@
 require("dotenv").config();
 
-import { Client, Intents } from "discord.js";
+import {
+  Client,
+  Collection,
+  CommandInteraction,
+  Intents,
+  Interaction,
+} from "discord.js";
+import { command, execute } from "./commands/ping";
 
-console.log("here");
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import { SlashCommandBuilder } from "@discordjs/builders";
+
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const commands = new Collection<
+  string,
+  {
+    builder: SlashCommandBuilder;
+    execute: (interaction: CommandInteraction) => Promise<void>;
+  }
+>().set(command.name, { builder: command, execute });
 
-// When the client is ready, run this code (only once)
-client.once("ready", () => {
-  console.log("Ready3!");
+const rest = new REST({ version: "9" }).setToken(
+  process.env.DISCORD_BOT_TOKEN!
+);
+rest.put(Routes.applicationCommands(process.env.DISCORD_BOT_CLIENT_ID!), {
+  body: commands.map((command) => command.builder.toJSON()),
 });
 
-// Login to Discord with your client's token
+client.once("ready", () => {
+  console.log("Bot is ready!");
+});
+
+client.on("interactionCreate", async (interaction) => {
+  console.log(interaction);
+  if (!interaction.isCommand()) return;
+
+  const command = commands.get(interaction.commandName);
+  if (!command) {
+    return await interaction.reply({
+      content: "Sorry, the command you send does not exist!",
+      ephemeral: true,
+    });
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
+  }
+});
+
 client.login(process.env.DISCORD_BOT_TOKEN);
